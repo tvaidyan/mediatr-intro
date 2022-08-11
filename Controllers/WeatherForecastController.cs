d8f04bb4d676ca624341e069e817043886b7a4d5
Tom Vaidyan <tvaidyan@gmail.com>
2022-08-10 22:22:50 -0500

    initial add

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mediatr_intro.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public WeatherForecastController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<WeatherForecastResponse> Get()
+    {
+        return await mediator.Send(new WeatherForecastRequest());
+    }
+}
---
+++ GetWeatherForecastHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+
+namespace mediatr_intro
+{
+	public class WeatherForecastRequest : IRequest<WeatherForecastResponse>
+	{
+		// your request params will go here
+	}
+
+	public class GetWeatherForecastHandler : IRequestHandler<WeatherForecastRequest, WeatherForecastResponse>
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		public async Task<WeatherForecastResponse> Handle(WeatherForecastRequest request, CancellationToken cancellationToken)
+        {
+            var weatherForecastResponse = new WeatherForecastResponse
+            {
+                WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            };
+            return await Task.FromResult(weatherForecastResponse);
+		}
+    }
+
+	public class WeatherForecastResponse
+	{
+		public IEnumerable<WeatherForecast> WeatherForecasts { get; set; } = new List<WeatherForecast>();
+	}
+}
+
